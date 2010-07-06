a0283ff82bc667f32f2a0b34d60eb66370afaacd
Laurent Desgrange <laurent@desgrange.net>
2010-07-06 23:21:24 +0200

    Fixed some warnings.

--- src/main/java/net/desgrange/pwad/ui/AboutDialog.java
+++ src/main/java/net/desgrange/pwad/ui/AboutDialog.java
@@ -23,19 +23,19 @@ import java.text.MessageFormat;
 import java.util.ResourceBundle;
 
 import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.LayoutStyle.ComponentPlacement;
 
 import net.desgrange.pwad.service.EnvironmentService;
 
-public class AboutDialog extends JDialog {
+public final class AboutDialog extends JDialog {
     private static final long serialVersionUID = -8568798361234817727L;
-    private final EnvironmentService environmentService;
+    private transient final EnvironmentService environmentService;
 
     public AboutDialog(final Frame parent, final EnvironmentService environmentService) {
         super(parent, Dialog.ModalityType.MODELESS);
@@ -58,29 +58,29 @@ public class AboutDialog extends JDialog {
         logoLabel.setName("logoLabel"); // NOI18N
 
         aboutLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        aboutLabel.setText(MessageFormat.format(ResourceBundle.getBundle("pwad/l10n/AboutDialog").getString("AboutDialog.aboutLabel.text"), new Object[] {environmentService.getVersion()})); // NOI18N
+        aboutLabel.setText(MessageFormat.format(ResourceBundle.getBundle("pwad/l10n/AboutDialog").getString("AboutDialog.aboutLabel.text"), new Object[] { environmentService.getVersion() })); // NOI18N
         aboutLabel.setName("aboutLabel"); // NOI18N
 
-        GroupLayout layout = new GroupLayout(getContentPane());
+        final GroupLayout layout = new GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(Alignment.LEADING)
-            .addGroup(Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(Alignment.TRAILING)
-                    .addComponent(logoLabel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 231, Short.MAX_VALUE)
-                    .addComponent(aboutLabel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 231, Short.MAX_VALUE))
-                .addContainerGap())
-        );
+                layout.createParallelGroup(Alignment.LEADING)
+                        .addGroup(Alignment.TRAILING, layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(layout.createParallelGroup(Alignment.TRAILING)
+                                        .addComponent(logoLabel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 231, Short.MAX_VALUE)
+                                        .addComponent(aboutLabel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 231, Short.MAX_VALUE))
+                                .addContainerGap())
+                );
         layout.setVerticalGroup(
-            layout.createParallelGroup(Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(logoLabel)
-                .addPreferredGap(ComponentPlacement.RELATED)
-                .addComponent(aboutLabel)
-                .addContainerGap())
-        );
+                layout.createParallelGroup(Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(logoLabel)
+                                .addPreferredGap(ComponentPlacement.RELATED)
+                                .addComponent(aboutLabel)
+                                .addContainerGap())
+                );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
